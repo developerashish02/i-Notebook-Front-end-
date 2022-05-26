df3f3ffe02c12f6207d0b788fc20149a6091af0f
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-05-26 10:58:51 +0530

    to take notes input

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,57 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import NoteContext from "../context/NoteContext";
+import Notes from "./Notes";
 function Home() {
 	return (
-		<div className="container my-3">
-			<h3>Add your note</h3>
-			<form>
-				<div className="mb-3">
-					<label for="exampleInputEmail1" className="form-label">
-						Email address
-					</label>
-					<input
-						type="email"
-						className="form-control"
-						id="exampleInputEmail1"
-						aria-describedby="emailHelp"
-					/>
-					<div id="emailHelp" className="form-text">
-						We'll never share your email with anyone else.
+		<>
+			<div className="container my-3">
+				<h3>Add your note</h3>
+				<form>
+					<div className="mb-3">
+						<label htmlFor="exampleInputEmail1" className="form-label">
+							Email address
+						</label>
+						<input
+							type="email"
+							className="form-control"
+							id="exampleInputEmail1"
+							aria-describedby="emailHelp"
+						/>
+						<div id="emailHelp" className="form-text">
+							We'll never share your email with anyone else.
+						</div>
 					</div>
-				</div>
-				<div className="mb-3">
-					<label for="exampleInputPassword1" className="form-label">
-						Password
-					</label>
-					<input
-						type="password"
-						className="form-control"
-						id="exampleInputPassword1"
-					/>
-				</div>
-				<div className="mb-3 form-check">
-					<input
-						type="checkbox"
-						className="form-check-input"
-						id="exampleCheck1"
-					/>
-					<label className="form-check-label" for="exampleCheck1">
-						Check me out
-					</label>
-				</div>
-				<button type="submit" className="btn btn-primary">
-					Submit
-				</button>
-			</form>
-		</div>
+					<div className="mb-3">
+						<label htmlFor="exampleInputPassword1" className="form-label">
+							Password
+						</label>
+						<input
+							type="password"
+							className="form-control"
+							id="exampleInputPassword1"
+						/>
+					</div>
+					<div className="mb-3 form-check">
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="exampleCheck1"
+						/>
+						<label className="form-check-label" htmlFor="exampleCheck1">
+							Check me out
+						</label>
+					</div>
+					<button type="submit" className="btn btn-primary">
+						Submit
+					</button>
+				</form>
+			</div>
+
+			{/* user notes */}
+			<div className="container">
+				<Notes />
+			</div>
+		</>
 	);
 }
 
