38cdebd4d5416aeab81b3cdb831f70b7791b5ac6
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-05-26 17:20:18 +0530

    import alert components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import NoteState from "./context/NoteState";
+import Alert from "./components/Alert";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<BrowserRouter>
 				<NoteState>
 					<Navbar />
+					<Alert />
 					<div className="container">
 						<Routes>
 							<Route exact path="/" element={<Home />} />
