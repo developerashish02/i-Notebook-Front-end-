803334b4a893f141e9b80da480dc7b2ed3d60179
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-05-29 00:45:17 +0530

    Add signup logic

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const host = "http://localhost:5000";
 
 function SignUp() {
-  return (
-    <div>
-      
-    </div>
-  )
+	let navigate = useNavigate();
+	const [userDetail, setuserDetail] = useState({
+		name: "",
+		email: "",
+		password: "",
+	});
+	const handleChnage = (e) => {
+		setuserDetail({ ...userDetail, [e.target.name]: e.target.value });
+	};
+
+	// handleSubmit
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		// fetch login api
+		const response = await fetch(`${host}/api/auth/createuser`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				name: userDetail.name,
+				email: userDetail.email,
+				password: userDetail.password,
+			}),
+		});
+
+		const json = await response.json();
+		// redirect to the home
+		localStorage.setItem("token", json.authtoken);
+		navigate("/");
+
+		setuserDetail({
+			name: "",
+			email: "",
+			password: "",
+		});
+	};
+	return (
+		<div className="container">
+			<form onSubmit={handleSubmit}>
+				<div className="mb-3">
+					<label htmlFor="name" className="form-label">
+						Full Name
+					</label>
+					<input
+						type="text"
+						className="form-control"
+						id="name"
+						onChange={handleChnage}
+						name="name"
+						value={userDetail.name}
+					/>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="exampleInputEmail1" className="form-label">
+						Email address
+					</label>
+					<input
+						type="email"
+						className="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+						onChange={handleChnage}
+						name="email"
+						value={userDetail.email}
+					/>
+					<div id="emailHelp" className="form-text">
+						We'll never share your email with anyone else.
+					</div>
+				</div>
+				<div className="mb-3">
+					<label htmlFor="Password" className="form-label">
+						Password
+					</label>
+					<input
+						type="password"
+						className="form-control"
+						id="password"
+						onChange={handleChnage}
+						name="password"
+						value={userDetail.password}
+					/>
+				</div>
+
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 
-export default SignUp
+export default SignUp;
