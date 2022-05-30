23fa23f39c6e983aa3393c5b91ac0d004dc29257
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-05-30 16:45:24 +0530

    video App demo

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,54 +2,54 @@ import { useLayoutEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const UserProfile = () => {
-    const [user, userDetail] = useState({});
-    const host = "http://localhost:5000";
-    const navigate = useNavigate();
-    useLayoutEffect(() => {
-        getUserDetail();
-    }, []);
+	const [user, userDetail] = useState({});
+	const host = "http://localhost:5000";
+	const navigate = useNavigate();
+	useLayoutEffect(() => {
+		getUserDetail();
+	}, []);
 
-    // handlelogout
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        navigate("/login");
-    };
+	// handlelogout
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		navigate("/login");
+	};
 
-    // get use details
-    const getUserDetail = async () => {
-        const response = await fetch(`${host}/api/auth/getuser`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token"),
-            },
-        });
-        const json = await response.json();
-        userDetail(json);
-    };
+	// get use details
+	const getUserDetail = async () => {
+		const response = await fetch(`${host}/api/auth/getuser`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"auth-token": localStorage.getItem("token"),
+			},
+		});
+		const json = await response.json();
+		userDetail(json);
+	};
 
-    return (
-        <div className="container w-50 my-4">
-            <div className=" d-flex justify-content-center align-items-center border border-success rounded-3 px-3 py-3">
-                <img
-                    src="https://cdn-icons-png.flaticon.com/128/236/236832.png"
-                    className="mx-2 px-3 "
-                    style={style}
-                />
-                <div className="card-body">
-                    <h3 className="card-title">{user.name}</h3>
-                    <h5>{user.email}</h5>
-                    <button className="btn btn-primary" onClick={handleLogout}>
-                        Log out
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="container w-50 my-4">
+			<div className=" d-flex justify-content-center align-items-center border border-success rounded-3 px-3 py-3">
+				<img
+					src="https://cdn-icons-png.flaticon.com/128/236/236832.png"
+					className="mx-2 px-3 "
+					style={style}
+				/>
+				<div className="card-body">
+					<h3 className="card-title">{user.name}</h3>
+					<h5>{user.email}</h5>
+					<button className="btn btn-primary" onClick={handleLogout}>
+						Log out
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 const style = {
-    height: "100",
-    width: "100",
-    cursor: "pointer",
+	height: "100",
+	width: "100",
+	cursor: "pointer",
 };
 export default UserProfile;
